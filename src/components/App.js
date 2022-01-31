8e26ce77fd067b6874d70806e608ef4d5b22ded1
Pranjal Mishra <pranjal.ug19.cs@nitp.ac.in>
2022-01-31 21:36:40 +0530

    add note functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (newNote) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  };
   return (
     <div>
       <Header />
+      <CreateArea onAdd={addNote} />
       {notes.map((note) => (
         <Note key={note.key} title={note.title} content={note.content} />
       ))}
